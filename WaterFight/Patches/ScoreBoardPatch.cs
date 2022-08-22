cf808efab0977d8c97abfcf025cb1fc5e956c9c3
A Haunted Army <extreme15@hotmail.co.uk>
2022-08-22 20:00:31 +0100

    setting scoreboard text to overflow so text doesn't get bumped down extra lines

---
+++ WaterFight/Patches/ScoreBoardPatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using HarmonyLib;
+
+namespace WaterFight.Patches
+{
+    [HarmonyPatch(typeof(GorillaScoreBoard))]
+    [HarmonyPatch("Awake", MethodType.Normal)]
+    internal class ScoreBoardPatch
+    {
+        internal static void Postfix(GorillaScoreBoard __instance)
+        {
+            __instance.boardText.horizontalOverflow = UnityEngine.HorizontalWrapMode.Overflow;
+        }
+    }
+}
